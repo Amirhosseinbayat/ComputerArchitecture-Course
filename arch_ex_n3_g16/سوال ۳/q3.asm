db825481be784241b96d18a75cee15c1ee803eb5
mohsenkamini <mohsenkamini@gmail.com>
2023-04-09 20:11:00 +0330

    q3.asm needs tests

--- arch_ex_n3_g16/سوال ۳/q3.asm
+++ arch_ex_n3_g16/سوال ۳/q3.asm
@@ -3,25 +3,25 @@
 addi s0,zero,10		# arr_size
 addi sp,sp,-40
 
-addi t2,zero,3
+addi t2,zero,1
 sw t2,36(sp)
-addi t2,zero,10
-sw t2,32(sp)
 addi t2,zero,2
-sw t2,28(sp)
+sw t2,32(sp)
 addi t2,zero,3
+sw t2,28(sp)
+addi t2,zero,4
 sw t2,24(sp)
-addi t2,zero,13
+addi t2,zero,5
 sw t2,20(sp)
-addi t2,zero,11
+addi t2,zero,6
 sw t2,16(sp)
-addi t2,zero,9
+addi t2,zero,7
 sw t2,12(sp)
 addi t2,zero,8
 sw t2,8(sp)
-addi t2,zero,5
+addi t2,zero,9
 sw t2,4(sp)
-addi t2,zero,4
+addi t2,zero,10
 sw t2,0(sp)
 
 add t1,zero,sp	# arr[]
@@ -30,9 +30,29 @@ addi s1,zero,1	# to check n with one
 MAIN: 
 
 	addi s2,zero,2 		# i = 2 
-	blt s2,s0,
-	
 FOR:	
+	bge s2,s0,DONE		# branch if i >= arr_size
+	lw a0,8(sp)		# a0 = arr[i]
+	jal IS_EVEN
+IF:	beq a0,zero,ELSE
+	lw a0,8(sp)		# a0 = arr[i]
+	lw a1,(sp)		# a1 = arr[i-2]
+	add t3,a0,a1		# t3 = arr[i] + arr [i-2]
+	sw t3,8(sp)		# arr[i] += arr[i-2]
+	j FI
+ELSE:
+	lw a1,4(sp)		# a1 = arr[i-1]
+	add t4,zero,a1		# i = a1
+	FOR_2:			# for (int i=a1 ; i > 0 ; i++ ) a0+=a0;
+		ble t4,zero,ROF_2
+		add a0,a0,a0
+	ROF_2:
+	sw a0,8(sp)		# arr[i]*arr[i-1] => arr[i]
+FI:
+	addi sp,sp,4		
+	addi,s2,s2,1
+	
+DONE:
 
 j END
 
