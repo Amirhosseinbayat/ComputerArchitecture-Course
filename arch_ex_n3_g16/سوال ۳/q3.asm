e29cd5a41b76c629e9bca79ce65b02d5c75bb41c
mohsenkamini <mohsenkamini@gmail.com>
2023-04-09 19:37:10 +0330

    updated q3.asm

--- arch_ex_n3_g16/سوال ۳/q3.asm
+++ arch_ex_n3_g16/سوال ۳/q3.asm
@@ -1,14 +1,40 @@
 
 
-addi t0,zero,10
-addi sp,sp,-10
+addi s0,zero,10		# arr_size
+addi sp,sp,-40
+
+addi t2,zero,3
+sw t2,36(sp)
+addi t2,zero,10
+sw t2,32(sp)
+addi t2,zero,2
+sw t2,28(sp)
+addi t2,zero,3
+sw t2,24(sp)
+addi t2,zero,13
+sw t2,20(sp)
+addi t2,zero,11
+sw t2,16(sp)
+addi t2,zero,9
+sw t2,12(sp)
+addi t2,zero,8
+sw t2,8(sp)
+addi t2,zero,5
+sw t2,4(sp)
+addi t2,zero,4
+sw t2,0(sp)
 
 add t1,zero,sp	# arr[]
-addi s0,zero,1	# to check n with one
+addi s1,zero,1	# to check n with one
 
 MAIN: 
 
-jr ra
+	addi s2,zero,2 		# i = 2 
+	blt s2,s0,
+	
+FOR:	
+
+j END
 
 
 IS_EVEN:
@@ -44,4 +70,4 @@ IS_ODD:
 	jal ra,IS_EVEN
 	jr ra
 
-
+END:
